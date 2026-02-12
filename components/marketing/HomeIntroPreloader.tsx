773c2154095b95723f8a8f0b811c2fe26ff48ab2
Remyremified-git <valentinobenedict033@gmail.com>
2026-02-12 15:29:47 -0800

    Slow and refine homepage preloader typography and cursor timing

--- components/marketing/HomeIntroPreloader.tsx
+++ components/marketing/HomeIntroPreloader.tsx
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useState } from 'react'
 import styles from '@/components/marketing/HomeIntroPreloader.module.css'
 
 const MESSAGE = ['WELCOME', 'TO', 'TRINITY IN ONE', 'INVESTMENTS'].join('\n')
-const TYPE_INTERVAL_MS = 95
+const TYPE_INTERVAL_MS = 135
 const HOLD_AFTER_DONE_MS = 700
 const FADE_OUT_MS = 760
 
